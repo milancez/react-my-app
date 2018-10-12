8b0b49177df3216d167d40809b9026e27731f668
milancez <milancez@gmail.com>
2018-10-12 15:27:58 +0200

    Complete my-app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
 import React, { Component } from 'react';
-import Ninjas from './Ninjas'
+import Ninjas from './Ninjas';
+import AddNinja from './AddNinja';
 
 class App extends Component {
+  state = {
+    ninjas : [
+      { name: 'Ryu', age: 30, belt: 'black', id: 1 },
+      { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
+      { name: 'Cristal', age: 25, belt: 'pink', id: 3 }
+    ]
+  }
+
+  AddNinja = (ninja) => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja];
+    this.setState({
+      ninjas: ninjas
+    })
+  }
+
+  deleteNinja = (id) => {
+    let ninjas = this.state.ninjas.filter(ninja => {
+      return ninja.id !== id
+    })
+    this.setState({
+      ninjas: ninjas
+    })
+  }
+
+  // Lifecycle phase - mounted
+  componentDidMount() {
+    console.log('Component mounted');
+  }
+
+  // Lifecycle phase - updated
+  componentDidUpdate(prevProps, prevState) {
+    console.log('component updated');
+    console.log(prevProps, prevState);
+  }
+
   render() {
     return (
       <div className="App">
         <h1>My first React app</h1>
-        <Ninjas />
+        <Ninjas ninjas={ this.state.ninjas } deleteNinja={this.deleteNinja} />
+        <AddNinja AddNinja={this.AddNinja} />
       </div>
     );
   }
--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,15 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Ninjas extends Component {
-  render() {
-    return (
-      <div className="ninja">
-        <div>Name: Ryu</div>
-        <div>Age: 30</div>
-        <div>Belt: Black</div>
+const Ninjas = ({ninjas, deleteNinja}) => {
+  const ninjaList = ninjas.map(ninja => {
+    return ninja.age > 20 ? (
+      <div className="ninja" key={ ninja.id }>
+        <div>Name: { ninja.name }</div>
+        <div>Age: { ninja.age }</div>
+        <div>Belt: { ninja.belt }</div>
+        <button onClick={() => {deleteNinja(ninja.id)}}>Delete ninja</button>
       </div>
-    );
-  }
+    ) : null;
+  })
+
+  return (
+    <div className="ninja-list">
+      { ninjaList }
+    </div>
+  );
 }
 
 export default Ninjas;
